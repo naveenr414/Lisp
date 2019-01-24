e78f5b8b6ebd97cee6cac412329036476958ad4b
naveenr414 <dsfan414@gmail.com>
2019-01-23 22:46:52 -0500

    Added more functions

--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,6 @@
 #include "functions.h"
 #include "operator.h"
+#include <algorithm> 
 
 int add(std::vector<int> a)
 { 
@@ -46,10 +47,50 @@ int identity(std::vector<int> a)
   return a.at(0);
 }
 
-Operator* add_operator = new Operator(add,{"ADD","+"});
-Operator* sub_operator = new Operator(sub,{"SUB","-"});
-Operator* mul_operator = new Operator(mul,{"MULTIPLY","*"});
-Operator* div_operator = new Operator(divide,{"DIVIDE","/"});
+int maximum(std::vector<int> a)
+{
+  int m = a.at(0);
+  for (int i = 1;i<a.size();i++)
+  {
+    m = std::max(m,a.at(i));
+  }
+  
+  return m;
+}
+
+int minimum(std::vector<int> a)
+{
+  int m = a.at(0);
+  for (int i = 1;i<a.size();i++)
+  {
+    m = std::min(m,a.at(i));
+  }
+  
+  return m;
+}
+
+int absolute(std::vector<int> a)
+{
+  return std::abs(a.at(0));
+}
+
+int rem(std::vector<int> a)
+{
+  return a.at(0)%a.at(1);
+}
+
+Operator* add_operator = new Operator(add,{"+"});
+Operator* sub_operator = new Operator(sub,{"-"});
+Operator* mul_operator = new Operator(mul,{"*"});
+Operator* div_operator = new Operator(divide,{"/"});
+Operator* max_operator = new Operator(maximum,{"max"});
+Operator* min_operator = new Operator(minimum,{"min"});
+Operator* rem_operator = new Operator(rem,{"rem"});
+Operator* abs_operator = new Operator(absolute,{"abs"});
 Operator* identity_operator = new Operator(identity,{});
 
-Operator* operator_list[] = {add_operator,sub_operator,mul_operator,div_operator,identity_operator,NULL};
+Operator* operator_list[] = {add_operator,sub_operator,
+                              mul_operator,div_operator,
+                              max_operator,min_operator,
+                              rem_operator,abs_operator,
+                              identity_operator,NULL};
--- functions.h
+++ functions.h
@@ -6,10 +6,18 @@ int sub(std::vector<int> a);
 int mul(std::vector<int> a);
 int divide(std::vector<int> a);
 int identity(std::vector<int> a);
+int maximum(std::vector<int> a);
+int minimum(std::vector<int> a);
+int absolute(std::vector<int> a);
+int rem(std::vector<int> a);
 
 extern Operator* add_operator;
 extern Operator* sub_operator;
 extern Operator* mul_operator;
 extern Operator* div_operator;
 extern Operator* identity_operator;
+extern Operator* max_operator;
+extern Operator* min_operator;
+extern Operator* abs_operator;
+extern Operator* rem_operator;
 extern Operator* operator_list[];
